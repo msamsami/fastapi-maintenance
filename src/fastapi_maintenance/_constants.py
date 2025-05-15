ab0b6d59a259e50c1f6a8ea60bfb68579080495b
Mehdi Samsami <mehdisamsami@live.com>
2025-05-15 17:38:39 +0330

    Make core an internal module, add function for registering middleware backend, remove context manager code

--- src/fastapi_maintenance/_constants.py
+++ src/fastapi_maintenance/_constants.py
@@ -9,8 +9,5 @@ FORCE_MAINTENANCE_MODE_OFF_ATTR = "force_maintenance_mode_off"
 # Default environment variable name
 MAINTENANCE_MODE_ENV_VAR_NAME = "FASTAPI_MAINTENANCE_MODE"
 
-# Default file name for local file backend
-MAINTENANCE_MODE_LOCAL_FILE_NAME = "maintenance_mode.txt"
-
 # Default JSON response content
 DEFAULT_JSON_RESPONSE_CONTENT = {"detail": "Service temporarily unavailable due to maintenance"}
