82aa912dd13c27e1d2948be12770728dd7e0cbeb
Mehdi Samsami <mehdisamsami@live.com>
2025-05-17 15:44:06 +0330

    Remove docs_src directory

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 uv run mypy src/fastapi_maintenance
-uv run ruff check src/fastapi_maintenance tests docs_src
-uv run ruff format src/fastapi_maintenance tests docs_src --check
+uv run ruff check src/fastapi_maintenance tests
+uv run ruff format src/fastapi_maintenance tests --check
